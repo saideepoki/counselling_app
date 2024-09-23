cbdc98a6381c779de43215f5c6c7d2daa184cf82
saideepoki <95339341+saideepoki@users.noreply.github.com>
2024-09-23 20:21:10 +0530

    added main screens structure in tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,29 +9,10 @@ export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
+    <Tabs>
+      <Tabs.Screen name="home" options={{ title: 'Home' }} />
+      <Tabs.Screen name="convo" options={{ title: 'Chat' }} />
+      <Tabs.Screen name="history" options={{ title: 'History' }} />
     </Tabs>
   );
 }
