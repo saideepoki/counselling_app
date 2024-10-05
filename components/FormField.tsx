7df917d8fc2a416d47ad50a16248d8df984dc49c
saideepoki <95339341+saideepoki@users.noreply.github.com>
2024-10-05 11:04:42 +0530

    designed a custom formfield

---
+++ components/FormField.tsx
@@ -0,0 +1,33 @@
+import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native'
+import React, { useState } from 'react'
+import { icons } from '@/constants';
+
+const FormField = ({title, value, placeholder, handleChangeText, otherStyles, keyboardType, ...props} : {title: string, value: string, placeholder?: string, handleChangeText: any, otherStyles: string, keyboardType?: string}) => {
+  const [showPassword, setShowPassword] = useState(false);
+  return (
+    <View className={`space-y-2 ${otherStyles}`}>
+      <Text className='text-base text-gray-100 font-pmedium'>{title}</Text>
+      <View className = 'w-full h-16 px-4 border-2 border-black-200 bg-black-100 rounded-2xl focus:border-cyan-500 items-center flex-row'>
+        <TextInput
+        className = 'flex-1 text-base text-white font-psemibold'
+        value = {value}
+        onChangeText={handleChangeText}
+        placeholder={placeholder}
+        placeholderTextColor='#7b7b8b'
+        secureTextEntry = {title === 'Password' && !showPassword}
+        />
+
+        {title === 'Password' && (
+          <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+            <Image
+            className='w-6 h-6'
+            resizeMode='contain'
+            source={icons.eyeHide}/>
+          </TouchableOpacity>
+        )}
+      </View>
+    </View>
+  )
+}
+
+export default FormField
